4f27364d0c71ef6ec20e9b54897a3eea9ae25092
CalSch <cal.schwick@gmail.com>
2022-12-08 21:24:45 -0500

    gave page 2 a title

--- pages/page2.tsx
+++ pages/page2.tsx
@@ -2,7 +2,7 @@ import Layout from "../components/layout"
 
 export default function Page2() {
   return (
-    <Layout>
+    <Layout title="Page 2">
       <h1>Hello, this is page 2</h1>
     </Layout>
   )
