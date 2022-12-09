d3a8e2cb4b86a1adbb74f1fe8d4adecb083c47e8
bradyinnovation <bradybbangasser@gmail.com>
2022-12-08 20:24:17 -0600

    next-auth int started

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+// DON'T WRITE YOUR WEB PAGES HERE
+
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
