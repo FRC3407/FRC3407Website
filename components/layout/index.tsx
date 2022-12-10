0ce4894a1980fcf423ca411a9c6447b979e863ac
CalSch <cal.schwick@gmail.com>
2022-12-09 20:01:22 -0500

    Added some accessibility

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -7,6 +7,7 @@ export default function Layout({ children, title }: { children: any, title: stri
         <div>
             <Head>
                 <title>{title+" | FRC 3407"}</title>
+                <meta name="description" content="The website for FRC 3407" />
             </Head>
             <Navbar/>
             {children}
