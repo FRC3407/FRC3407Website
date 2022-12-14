53000dc17b06c6f8894bf6ea79b46cc5569c4958
bradyinnovation <bradybbangasser@gmail.com>
2022-12-14 11:44:07 -0600

    Added Error Handling

--- pages/error/[...message].tsx
+++ pages/error/[...message].tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useRouter } from "next/router";
+import Layout from "../../components/layout";
 
-export default function Error(...things: any[]) {
-  console.log(things);
+export default function Error() {
+  const { message = ["No Code Provided", "No Message Provided"] } =
+    useRouter().query;
 
-  return <p>hi</p>;
+  let code: string = "No Code Provided";
+  let displayMessage: string = "No Message Provided";
+
+  if (message.length <= 1) {
+    if (isNaN(parseInt(message[0]))) displayMessage = message[0];
+    else code = message[0];
+  } else {
+    (Array.isArray(message) ? message : [message]).forEach((messElement) => {
+      if (isNaN(parseInt(messElement))) {
+        displayMessage = messElement;
+      } else {
+        code = messElement;
+      }
+    });
+  }
+
+  return (
+    <Layout title={"Error: " + code}>
+      <div>
+        <h1>There seems to be an error :(</h1>
+        <h2>Code: {code}</h2>
+        <h2>Message: {displayMessage}</h2>
+      </div>
+    </Layout>
+  );
 }
--- pages/error/index.tsx
+++ pages/error/index.tsx
@@ -1,3 +1,17 @@
+import { useRouter } from "next/router";
+import Layout from "../../components/layout";
+
 export default function Error() {
-  return <p>Error?</p>;
+  const { code = "No Code Provided", message = "No Message Provided" } =
+    useRouter().query;
+
+  return (
+    <Layout title={"Error: " + code}>
+      <div>
+        <h1>There seems to be an error :(</h1>
+        <h2>Code: {code}</h2>
+        <h2>Message: {message}</h2>
+      </div>
+    </Layout>
+  );
 }
