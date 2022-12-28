5feb8b4ccf81504b9f59ddf04d4873ef4e4f7b32
Brady Bangasser <bradybbangasser@gmail.com>
2022-12-28 07:34:46 -0600

    PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,47 +17,6 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <ErrorBoundary>
-        <Head>
-          <meta name="application-name" content="FRC Team 3407" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
-          <meta
-            name="description"
-            content="The Official Website of the Wild Cards (FRC Team 3407)"
-          />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="msapplication-TileColor" content="#2B5797" />
-          <meta name="msapplication-tap-highlight" content="no" />
-          <meta name="theme-color" content="#000000" />
-
-          <link rel="manifest" href="/manifest.json" />
-
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
-          <meta
-            name="twitter:description"
-            content="Best PWA App in the world"
-          />
-          <meta
-            name="twitter:image"
-            content="https://yourdomain.com/icons/android-chrome-192x192.png"
-          />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
-          <meta property="og:url" content="https://yourdomain.com" />
-          <meta
-            property="og:image"
-            content="https://yourdomain.com/icons/apple-touch-icon.png"
-          />
-        </Head>
         {(Component as ExtendedComponent).auth ? (
           <AccessControlLayer>
             {/* @ts-ignore-error */}
