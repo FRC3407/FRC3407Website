d38ac7f265e14f56e0a4b3fdd2f73d7d8f3ea0d9
bradyinnovation <bradybbangasser@gmail.com>
2023-01-07 08:45:33 -0600

    Finding fs info

--- pages/bradys-test-world.tsx
+++ pages/bradys-test-world.tsx
@@ -2,9 +2,11 @@ import Calendar from "@components/calendar";
 import DynamicGallery from "@components/dynamicImgGallery";
 import importImages from "@components/dynamicImgGallery/import";
 import Layout from "@components/layout";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import * as fs from "fs/promises"
 
-export default function BradysTestWorld() {
+export default function BradysTestWorld({ log }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(log)
   return (
     <Layout title="Brady's Test World">
       <h1 className="centeredText">Brady&apos;s Test World</h1>
@@ -21,12 +23,14 @@ export default function BradysTestWorld() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps<{}> = async () => {
-  const images = await importImages("gallery");
+export const getServerSideProps: GetServerSideProps<{ log: string[] }> = async () => {
+  // const images = await importImages("gallery");
 
-  console.log(images)
+  // console.log(images)
 
   return {
-    props: {},
+    props: {
+      log: await fs.readdir(process.cwd())
+    },
   };
 };
