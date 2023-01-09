5a2001c0f901b5a6d1bbf86e893c0ba5087bf0cb
bradyinnovation <bradybbangasser@gmail.com>
2023-01-09 09:27:33 -0600

    public

--- pages/api/static-manifest.ts
+++ pages/api/static-manifest.ts
@@ -6,7 +6,6 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<string[] | string>
   ) {
-    //return res.status(200).json(await fs.readdir(path.join(process.cwd(), "json")));
-    res.status(200).send(path.join(process.cwd(), "json"))
+    return res.status(200).json(await fs.readdir(path.join(process.cwd(), "public")));
   }
   
