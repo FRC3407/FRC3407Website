32e843aa606fd140f7300f11d9119f9ebf24da3d
bradyinnovation <bradybbangasser@gmail.com>
2023-01-09 09:24:41 -0600

    path

--- pages/api/static-manifest.ts
+++ pages/api/static-manifest.ts
@@ -4,8 +4,9 @@ import path from "path";
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<string[]>
+    res: NextApiResponse<string[] | string>
   ) {
-    return res.status(200).json(await fs.readdir(path.join(process.cwd(), "json")));
+    //return res.status(200).json(await fs.readdir(path.join(process.cwd(), "json")));
+    res.status(200).send(path.join(process.cwd(), "json"))
   }
   
