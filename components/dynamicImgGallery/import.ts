367ca73779d7bd3893b8a5e9702d717a8872506c
bradyinnovation <bradybbangasser@gmail.com>
2023-01-09 19:47:28 -0600

    sync

--- components/dynamicImgGallery/import.ts
+++ components/dynamicImgGallery/import.ts
@@ -2,16 +2,16 @@ import * as fs from "fs/promises";
 import * as fss from "fs"
 import path from "path";
 
-const getImages = async (folder: string) => {
+const getImages = async (...folder: string[]) => {
 
   let files = await fs.readdir(
-    path.join(process.cwd(), "public", "static", "images", "dynamic", folder)
+    path.join(process.cwd(), "public", "static", "images", "dynamic", ...folder)
   );
 
-  files.map((file) => {
-    if (fss.statSync(path.join(process.cwd(), "public", "static", "images", "dynamic", folder, file)).isDirectory()) console.log(file)
+  await Promise.all(files.map(async (file) => {
+    if ((await fs.stat(path.join(process.cwd(), "public", "static", "images", "dynamic", ...folder, file))).isDirectory()) console.log(file)
     return file
-  })
+  }))
   
 
   return files
