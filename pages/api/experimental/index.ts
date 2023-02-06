0995dba9d7d2759e8b40f1fe0a5d77716b382388
bradyinnovation <bradybbangasser@gmail.com>
2023-02-06 16:45:32 -0600

    api work

--- pages/api/experimental/index.ts
+++ pages/api/experimental/index.ts
@@ -1,14 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const frcUrl = new URL("https://frc-api.firstinspires.org/v3.0/2023")
-  frcUrl.searchParams.append("authorization", "YnJhZHliYW5nYXNzZXI6NzM5Y2ZlOTEtYzZhNy00MTU4LWExMGMtMjBlZjI5NDNkN2E4")
-  console.log(frcUrl.toString())
-  const frcRes = await fetch(frcUrl, {
-    headers: {
-      authorization: "YnJhZHliYW5nYXNzZXI6NzM5Y2ZlOTEtYzZhNy00MTU4LWExMGMtMjBlZjI5NDNkN2E4"
-    }
-  })
-  console.log("here", frcRes)
-  return res.status(200).send(frcRes.status)
+  
 }
