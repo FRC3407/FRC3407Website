787af4a4ba0e4ccd85b5639d6e7250eacff4e9ad
Brady Bangasser <bradybbangasser@gmail.com>
2023-02-21 20:11:36 -0600

    sync

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,30 +3,58 @@ import styles from "../../styles/components/Footer.module.scss";
 import GithubIcon from "@mui/icons-material/GitHub";
 import Avatar from "@mui/material/Avatar";
 
-
-
-const LinkColumn = (columnName: string, links: [string, string][]) => {
+const LinkColumn = (
+  columnName: string,
+  links: [string, string][],
+  key: string
+) => {
   return (
-    <div className={styles.linkColumn}>
+    <div className={styles.linkColumn} key={key}>
       <h3 className={styles.linkColumnHeader}>{columnName}</h3>
-      {links.map(([displayUrl, url]) => <a key={displayUrl} href={url} className={styles.linkColumnLink}>{displayUrl}</a>)}
+      {links.map(([displayUrl, url]) => (
+        <a key={displayUrl} href={url} className={styles.linkColumnLink}>
+          {displayUrl}
+        </a>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-const links: [string, [string, string][]][] = [["Hello", [["hello", "https://hello.com"]]]]
+const links: [string, [string, string][]][] = [
+  [
+    "Media",
+    [
+      ["Github", "https://github.com/FRC3407"],
+      ["Blue Alliance", "https://www.thebluealliance.com/team/3407"],
+    ],
+  ],
+  [
+    "About Us",
+    [
+      ["About Us", "/about"],
+      ["Sponsors", "/sponsors"],
+    ],
+  ],
+];
 
 const Footer: NextComponentType = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.upperFooter}>
         <div className={styles.socialMediaContainer}>
-          <a><Avatar className={styles.githubIcon}><GithubIcon fontSize="inherit" /></Avatar></a>
+          <a>
+            <Avatar className={styles.githubIcon}>
+              <GithubIcon fontSize="inherit" />
+            </Avatar>
+          </a>
         </div>
         <h4>Official Website of FRC Team 3407</h4>
       </div>
       <div className={styles.lowerFooter}>
-        {links.map(([title, links]) => LinkColumn(title, links))}
+        <h3>Important Links</h3>
+        <div className={styles.links}>
+          {links.map(([title, links]) => LinkColumn(title, links, title))}
+        </div>
       </div>
     </footer>
   );
---
+++ pages/about.tsx
@@ -0,0 +1,5 @@
+import Layout from "@components/layout";
+
+export default function About() {
+  return <Layout title="About Us"></Layout>;
+}
