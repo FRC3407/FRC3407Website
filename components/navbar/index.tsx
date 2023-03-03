84413065c34937158452744db9f68265bf9f6b5a
CalSch <calschwick@calschwick.net>
2023-03-02 20:07:51 -0600

    changed styles

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -18,13 +18,13 @@ const Navbar: NextComponentType = () => {
     <div>
       <div className={style.nav}>
         <div className={style.profilePictureContainer}>
-          <Image tabIndex={0} onClick={profileClick} src="/static/images/assets/johnl.jpg" alt="Profile picture" fill className={style.profilePicture} />
           <div id="profileMenu" className={style.profileMenu} hidden>
             <h1>Menu</h1>
             <a href="#">calvin</a>
             <a href="#">is</a>
             <a href="#">epic</a>
           </div>
+          <Image tabIndex={0} onClick={profileClick} src="/static/images/assets/johnl.jpg" alt="Profile picture" fill className={style.profilePicture} />
         </div>
         <NavLink href="/">Home</NavLink>
         <NavLink href="/calendar">Calendar</NavLink>
