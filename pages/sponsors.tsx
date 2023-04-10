e48a91c52630c9918e6fbfc3acaef908ae8d98eb
Brady Bangasser <bradybbangasser@gmail.com>
2023-04-10 07:28:20 -0500

    sponsor image improvement

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -18,6 +18,7 @@ export default function Sponsors() {
               width: "auto",
               height: "auto"
             }}
+            priority
             
             // fill
           />
