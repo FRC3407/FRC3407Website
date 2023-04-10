dee0ffb3439a18d1280a9775f2f7d02ddf0cb938
Brady Bangasser <70297830+BradyBangasser@users.noreply.github.com>
2023-04-10 07:22:14 -0500

    Calvin dev (#18)
    * changed the mobile navbar menu icon
    
    * Added new sponsors
    
    ---------
    
    Co-authored-by: CalSch <cal.schwick@gmail.com>
    Co-authored-by: Cal Schwick <35741152+CalSch@users.noreply.github.com>

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -1,19 +1,27 @@
 import Layout from "@components/layout";
 import sponsors from "json/sponsors.json";
 import Image from "next/image";
+import style from "styles/pages/Sponsors.module.scss"
 
 export default function Sponsors() {
   return (
     <Layout title="Sponsors">
       {sponsors.map((sponsor) => (
-        <div key={sponsor.name}>
+        <a href={sponsor.link} key={sponsor.name} className={style.sponsorImageContainer}>
           <Image
             src={sponsor.image}
             alt={`${sponsor.name} logo`}
-            width={400}
-            height={400}
+            width={300}
+            height={300}
+            className={style.sponsorImage}
+            style={{
+              width: "auto",
+              height: "auto"
+            }}
+            
+            // fill
           />
-        </div>
+        </a>
       ))}
     </Layout>
   );
