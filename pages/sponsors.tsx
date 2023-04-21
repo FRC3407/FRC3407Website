f1c0196e35f08a65c3be5e4f7e850377471ff1cf
CalSch <cal.schwick@gmail.com>
2023-04-20 19:24:38 -0500

    Updated sponsors page

--- pages/sponsors.tsx
+++ pages/sponsors.tsx
@@ -6,23 +6,27 @@ import style from "styles/pages/Sponsors.module.scss"
 export default function Sponsors() {
   return (
     <Layout title="Sponsors">
-      {sponsors.map((sponsor) => (
-        <a href={sponsor.link} key={sponsor.name} className={style.sponsorImageContainer}>
-          <Image
-            src={sponsor.image}
-            alt={`${sponsor.name} logo`}
-            width={300}
-            height={300}
-            className={style.sponsorImage}
-            style={{
-              width: "auto",
-              height: "auto"
-            }}
-            
-            // fill
-          />
-        </a>
-      ))}
+      <div className={style.sponsorsContainer}>
+        {sponsors.map((sponsor) => (
+          <a href={sponsor.link} key={sponsor.name} className={style.sponsorImageContainer}>
+            <p>{sponsor.name}</p>
+            <Image
+              src={sponsor.image}
+              alt={`${sponsor.name} logo`}
+              width={300}
+              height={300}
+              className={style.sponsorImage}
+              style={{
+                width: "auto",
+                height: "auto"
+              }}
+              priority
+              
+              // fill
+            />
+          </a>
+        ))}
+      </div>
     </Layout>
   );
 }
